acb686bc9398edf938612de36e7f36867a12beec
zhuzm <zhuzaoming@inke.cn>
2021-01-31 10:40:46 +0800

    我的小程序第一次代码提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello zzm',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
